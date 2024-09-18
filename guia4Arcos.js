59df16c47ea975def3144cad26e6fd3c51a89eec
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2024-09-17 22:34:20 -0600

    Guia 4: Se crean arcos con la funcion arc(x,y,radio,angulo Inicio, angulo final) en radianes los angulos

--- guia4Arcos.js
+++ guia4Arcos.js
@@ -7,14 +7,25 @@ window.onload = function(){
             ctx.lineWidth = 25;
             ctx.strokeStyle = "yellow";
 
-            //iniciamos la linea 
+            //iniciamos la arco
             ctx.beginPath();
-            ctx.moveTo(50,100);
-            ctx.lineTo(100,50);
-            ctx.lineTo(150,100);
-            ctx.lineTo(100,150);
-
+            //y,x, radio, angulo de salida, angulo de finalizacion 
+            ctx.arc( 100, 150, 50, 1.1*Math.PI,1.9*Math.PI);
             ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(250,150, 50, 1.1 *Math.PI, 1.9 * Math.PI, true  );
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.arc(300,300, 50, 0, 2 * Math.PI );
+            ctx.fill();
+            ctx.stroke();
+
+            
+
+// Traza el arco
+ctx.stroke();
 
            
         }else{

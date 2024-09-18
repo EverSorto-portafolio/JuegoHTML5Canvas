acf44eb0fc701bb150f35e47322a5a2e9fb365aa
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2024-09-18 00:07:28 -0600

    Guia 4: Se crean un espiral

--- js/Espiral.js
+++ js/Espiral.js
@@ -4,18 +4,23 @@ window.onload = function(){
     if(canvas && canvas.getContext){
         let ctx = canvas.getContext("2d");
         if(ctx){
-            //definimos los colores 
-            ctx.fillStyle = "yelow";
-            ctx.strokeStyle = "#ff0000";
-            ctx.lineWidth = 5;
-            // x, y , w,h
-            ctx.fillRect( 50,50,100,100);
-            ctx.strokeRect(50,50,100,100);
+           let radio = 0, angulo = 0;
+           ctx.strokeStyle= "orange";
+           ctx.beginPath();
+           ctx.moveTo(canvas.width/2, canvas.height/2);
 
-            //Crear un segundo Cuadrado
-            ctx.fillStyle = "rgb(0,42,255)";
-            ctx.fillRect( 200,50,100,100);
-            ctx.strokeRect(200,50,100,100);
+           for(
+            let i = 0; // declaramos el inicio
+            i <200;  // se detendra en 199
+            i++ // i aumentara 1 en cada iteracion 
+            ){
+                radio  += 0.75; 
+                angulo += (Math.PI*2)/50; // angulo aumentars segun aumente las iteraciones
+                let x = canvas.width/2 + radio * Math.cos(angulo);  // mueve la x de manera continua sobre el eje x  
+                let y =  canvas.height/2 + radio * Math.sin(angulo); // mueve la linea de manera continua en el eje y 
+                ctx.lineTo(x,y); 
+           }
+           ctx.stroke();
 
         }else{
            alert("Navegador no soporta canvas 2D");

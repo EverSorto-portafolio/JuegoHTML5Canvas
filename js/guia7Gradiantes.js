dc00640413b8537c2d2209ae89adac0e26db11ad
EverSorto-portafolio <everalfredo.sorto@gmail.com>
2024-09-24 21:56:25 -0600

    Guia 7 parte 1 gradiantes

---
+++ js/guia7Gradiantes.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    let canvas = document.getElementById("marco");
+    if (canvas && canvas.getContext) {
+        let ctx = canvas.getContext("2d");
+        if (ctx) {
+
+            let gradiente = ctx.createLinearGradient(0, 0, canvas.width, 0); // Creamos los elementos que sera la gradiente.
+            gradiente.addColorStop(0, "blue",);
+            gradiente.addColorStop(0.5, "white",);
+            gradiente.addColorStop(1, "blue",);
+
+            ctx.fillStyle = gradiente;
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+                }
+    } else {
+        alert("canvas no soportado");
+    }
+}
